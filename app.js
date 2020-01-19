04f7d703d82f9a2302cea07d92d460e95a98fb40
ericsudhartio <*ericsudhartio2509@gmail.com>
2020-01-19 12:14:10 +0700

    production db

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ app.use(require('express').json());
 app.use(require('express').urlencoded({ extended: false }));
 
 //development
-require('mongoose').connect(`mongodb://localhost/freelance-${process.env.NODE_ENV}`, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: true })
-  .then(_ => console.log(`mongodb development now connected`, _.connections[0].name))
-  .catch(console.log)
+// require('mongoose').connect(`mongodb://localhost/freelance-${process.env.NODE_ENV}`, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: true })
+//   .then(_ => console.log(`mongodb development now connected`, _.connections[0].name))
+//   .catch(console.log)
 
 // production
-// require('mongoose').connect(`mongodb+srv://ericsudhartio:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}.mongodb.net/${process.env.MONGODB_URL}?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: true })
-//   .then(_ => console.log('mongodb production now connected, name -> ', _.connections[0].name, ' : host ->', _.connections[0].host))
-//   .catch(console.log)
+require('mongoose').connect(`mongodb+srv://ericsudhartio:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}.mongodb.net/${process.env.MONGODB_URL}?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: true })
+  .then(_ => console.log('mongodb production now connected, name -> ', _.connections[0].name, ' : host ->', _.connections[0].host))
+  .catch(console.log)
 
 app.use('/', require('./routes'));
 
