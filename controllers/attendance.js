3f5100c65feea3273385365b950645a561db7cdb
ericsudhartio <*ericsudhartio2509@gmail.com>
2020-01-20 20:22:59 +0700

    fix attendance

--- controllers/attendance.js
+++ controllers/attendance.js
@@ -5,7 +5,7 @@ module.exports = {
     try {
       const attendance = await Att.find(),
         date = new Date(),
-        start_image = req.body;
+        { start_image } = req.body;
       let pass = attendance.filter(el => el.date === date.toDateString())
       if( pass.length === 0 ) res.status(201).json({ attendance: await (await Att.create({ UserId: req.loggedUser.id, start_image })).populate('UserId') }) 
       else next({ status: 400, msg: 'Absent can only be once a day'})
