f7491bf93e5327d484bc8d4de71c086908e70ba3
ericsudhartio <*ericsudhartio2509@gmail.com>
2020-02-09 10:47:41 +0700

    update time from backend

--- routes/time.js
+++ routes/time.js
@@ -2,6 +2,6 @@ const { date } = require('../helpers'),
   { auth } = require('../middlewares'),
   Route = require('express').Router();
 
-Route.get('/', auth.authentication, (req, res,next) => res.status(200).json({ time: date() }))
+Route.get('/', auth.authentication, (req, res,next) => res.status(200).json({ time: date().toLocaleTimeString() }))
 
 module.exports = Route;
