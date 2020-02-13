44d5146a41aaaec61119927dbb3638e1952e681a
ericsudhartio <*ericsudhartio2509@gmail.com>
2020-02-13 19:34:25 +0700

    command all done in graphql

--- routes/correction.js
+++ routes/correction.js
@@ -2,12 +2,12 @@ const { CorrectionController: { createCorrection, getOneCorrection, getUserCorre
   { auth: { matchPin, isStaff } } = require('../middlewares')
   Route = require('express').Router();
 
-Route.get('/', getUserCorrection);
-Route.get('/search', findFilter);
-Route.post('/inreq', matchPin, seeAllRequestIn);
-Route.get('/:id', isStaff, getOneCorrection);
-Route.post('/', createCorrection);
-Route.patch('/:id/:res', matchPin, responseCorrection);
+Route.get('/', getUserCorrection); //*
+Route.get('/search', findFilter); //*
+Route.post('/inreq', matchPin, seeAllRequestIn); //*
+Route.get('/:id', isStaff, getOneCorrection); //*
+Route.post('/', createCorrection); //*
+Route.patch('/:id/:res', matchPin, responseCorrection); //*
 
 
 module.exports = Route;
--- routes/user.js
+++ routes/user.js
@@ -2,17 +2,17 @@ const Route = require('express').Router(),
   { UserController: { signin, signup, checkSignin, changePassword, updatePin, updateImage, forgotPassword, confirmSecretCode, filterFindUser, allEmployee } } = require('../controllers'),
   { auth: { authentication, checkSecretCode, isMaster, isEmployee, isStaff } } = require('../middlewares')
 
-Route.post('/signin', signin);
-Route.post('/forgot', forgotPassword);
-Route.post('/forgot/confirm', checkSecretCode, confirmSecretCode);
+Route.post('/signin', signin); // *
+Route.post('/forgot', forgotPassword); //*
+Route.post('/forgot/confirm', checkSecretCode, confirmSecretCode); //*
 
 Route.use( authentication );
-Route.get('/', checkSignin);
-Route.get('/employee', authentication, isEmployee, allEmployee);
-Route.post('/find', authentication, isEmployee, filterFindUser);
-Route.post('/signup', isMaster, signup);
-Route.post('/change', changePassword);
-Route.put('/change/pin', isStaff, updatePin);
-Route.post('/upload', updateImage);
+Route.get('/', checkSignin); // *
+Route.get('/employee', authentication, isEmployee, allEmployee); // *
+Route.post('/find', authentication, isEmployee, filterFindUser); // *
+Route.post('/signup', isMaster, signup); // *
+Route.post('/change', changePassword); //*
+Route.post('/upload', updateImage); //*
+Route.put('/change/pin', isStaff, updatePin); //*
 
 module.exports = Route;
